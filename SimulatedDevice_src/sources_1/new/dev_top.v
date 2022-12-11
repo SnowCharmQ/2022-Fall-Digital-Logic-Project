71c53c6a74d9e1eeac51af31e0db4b838c32ba7b
SnowCharm <619022098@qq.com>
2022-12-11 17:27:28 +0800

    update constaint

--- SimulatedDevice_src/sources_1/new/dev_top.v
+++ SimulatedDevice_src/sources_1/new/dev_top.v
@@ -31,6 +31,10 @@ module SimulatedDevice(
     input move_backward_signal,
     input place_barrier_signal,
     input destroy_barrier_signal,
+    input power,
+    input clutch,
+    input throttle,
+    input brake,
     output front_detector,
     output back_detector,
     output left_detector,
@@ -39,9 +43,9 @@ module SimulatedDevice(
     wire [7:0] in = {2'b10, destroy_barrier_signal, place_barrier_signal, turn_right_signal, turn_left_signal, move_backward_signal, move_forward_signal};
     wire [7:0] rec;
     assign front_detector = rec[0];
-    assign back_detector = rec[1];
-    assign left_detector = rec[2];
-    assign right_detector = rec[3];
+    assign left_detector = rec[1];
+    assign right_detector = rec[2];
+    assign back_detector = rec[3];
     
     uart_top md(.clk(sys_clk), .rst(0), .data_in(in), .data_rec(rec), .rxd(rx), .txd(tx));
     
