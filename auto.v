a57e29c095c4300e814cf587aa9be5b93bb72fe1
SnowCharm <619022098@qq.com>
2023-01-07 15:32:27 +0800

    update indent in auto.v

--- auto.v
+++ auto.v
@@ -17,93 +17,122 @@ output reg[1:0] next_state,output reg[3:0] next_moving_state);
     divclk myclk(sys_clk,clk_ms,clk_20ms,clk_16x,clk_x);
     //divclk clkk(.sys_clk(sys_clk),.clk_20ms(btnclk));
   
-    always@(turn_detector) begin
-    if(state==MOVE) begin
-    de_beacon=1'b0;pl_beacon=1'b0;
-    state=TURN;//turn=front;
-    case(turn_detector)
-    ~left: turn=left;//left is okay,others' stucked
-    ~right:turn=right;
-    ~back: begin de_beacon=1'b1;state=WAIT;end
-    //
-    4'b0000: begin turn=right;pl_beacon=1'b1;end //lrbf okay
-    left: begin turn=right;pl_beacon=1'b1;end
-    right:begin turn=front;pl_beacon=1'b1;end
-    front: begin turn=right;pl_beacon=1'b1;end
-    back:begin turn=right;pl_beacon=1'b1;end
-    //two dirctions bfrl(okay:1'b0) 6coniditions
-    //bf
-    4'b0101: begin turn=left;end//bl
-    4'b1001: begin turn=right;end//br
-    4'b0011: begin turn=right;end//rl
-    4'b1010: begin turn=right;end//fr
-    4'b0110: begin turn=front;end//fl
-    default: begin turn=front;end
-    endcase
-    end
-    else if(state==WAIT) begin
-      state=MOVE;
-      turn=front;
-    end
+    always@(sys_clk) begin
+      if (state == MOVE) begin
+        de_beacon <= 1'b0;
+        pl_beacon <= 1'b0;
+        state <= TURN;//turn=front;
+        case (turn_detector)
+        ~left: turn <= left;//left is okay,others' stucked
+        ~right: turn <= right;
+        ~back: begin 
+          de_beacon <= 1'b1;
+          state <= WAIT;
+        end
+        4'b0000: begin 
+          turn <= right;
+          pl_beacon <= 1'b1;
+        end //lrbf okay
+        left: begin 
+          turn <= right;
+          pl_beacon <= 1'b1;
+        end
+        right: begin 
+          turn <= front;
+          pl_beacon <= 1'b1;
+        end
+        front: begin 
+          turn <= right;
+          pl_beacon <= 1'b1;
+        end
+        back: begin 
+          turn <= right;
+          pl_beacon <= 1'b1;
+        end
+        //two dirctions bfrl(okay:1'b0) 6coniditions
+        //bf
+        4'b0101: turn <= left;//bl
+        4'b1001: turn <= right;//br
+        4'b0011: turn <= right;//rl
+        4'b1010: turn <= right;//fr
+        4'b0110: turn <= front;//fl
+        default: turn <= front;
+      endcase
+      end
+      else if (state == WAIT) begin
+        state <= MOVE;
+        turn <= front;
+      end
     end
     
-    always@* begin
+    always@(*) begin
       case(state) 
-        TURN:begin
-          if(turn_time_cnt==turn_period) begin
-            state=MOVE;
-            turn_time_cnt=8'd0;
-            end
-          else begin
-            state=TURN;
-            turn_time_cnt=turn_time_cnt;
-            end
+        TURN: begin
+          if(turn_time_cnt == turn_period) begin
+            state = MOVE;
+            turn_time_cnt = 8'd0;
           end
+          else begin
+            state = TURN;
+            turn_time_cnt = turn_time_cnt;
+          end
+        end
         MOVE: begin
-          if(move_time_cnt==move_period) begin
-            state=WAIT;
-            move_time_cnt=8'd0;
+          if(move_time_cnt == move_period) begin
+            state = WAIT;
+            move_time_cnt = 8'd0;
             end
           else begin
-            state=MOVE;
-            move_time_cnt=move_time_cnt;
-            end
+            state = MOVE;
+            move_time_cnt = move_time_cnt;
           end
-        WAIT:begin
-          if(wait_time_cnt==wait_period) begin
-            state=MOVE;
-            wait_time_cnt=8'd0;
-            end
+        end
+        WAIT: begin
+          if(wait_time_cnt == wait_period) begin
+            state = MOVE;
+          end
           else begin 
-            state=WAIT;
-            wait_time_cnt=wait_time_cnt;
-            end
+            state = WAIT;
           end
+        end
       endcase
     end
     
     always@(posedge clk_20ms) begin
       if(~rst) begin
-        turn_time_cnt<=8'b0;
-        move_time_cnt<=8'b0;
-        pl_beacon<=1'b0;pl_beacon_sig<=1'b0;//
-        de_beacon<=1'b0;de_beacon_sig<=1'b0;
-        state<=MOVE;next_state<=MOVE;//next_state<=state;
-        turn<=front;next_moving_state<=front;
-        end
+        turn_time_cnt <= 8'b0;
+        move_time_cnt <= 8'b0;
+        pl_beacon <= 1'b0;
+        pl_beacon_sig <= 1'b0;//
+        de_beacon <= 1'b0;
+        de_beacon_sig <= 1'b0;
+        state <= MOVE;
+        next_state <= MOVE;//next_state<=state;
+        turn <= front;
+        next_moving_state <= front;
+      end
       else begin
-        next_moving_state<=turn;
-        next_state<=state;
-        de_beacon_sig<=de_beacon;
-        if(state==TURN) turn_time_cnt<=turn_time_cnt+5'd20;
-        else if(state==MOVE) begin
+        next_moving_state <= turn;
+        next_state <= state;
+        de_beacon_sig <= de_beacon;
+        if(state == TURN) begin
+          turn_time_cnt <= turn_time_cnt + 5'd20;
+        end 
+        else if(state == MOVE) begin
           if(pl_beacon) begin
-            if(move_time_cnt==move_period) begin pl_beacon_sig<=pl_beacon; pl_beacon<=1'b0;end
-            else move_time_cnt<=move_time_cnt+5'd20;//
+            if (move_time_cnt == move_period) begin 
+              pl_beacon_sig <= pl_beacon; 
+              pl_beacon <= 1'b0;
             end
+            else begin
+              move_time_cnt <= move_time_cnt + 5'd20;
+            end 
           end
-        else if(state==WAIT) wait_time_cnt<=wait_time_cnt+5'd20;
         end
+        else if (state == WAIT) begin
+          wait_time_cnt <= wait_time_cnt + 5'd20;
+        end 
+      end
     end
      
 endmodule
